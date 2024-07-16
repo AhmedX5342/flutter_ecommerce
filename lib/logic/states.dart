dc1ea5aaa0acf18cba91ad23e930995b116ea455
Ahmed Elgharabawy <73799038+AhmedX5342@users.noreply.github.com>
2024-07-16 20:47:14 +0300

    Delete lib/logic/states.dart

--- lib/logic/states.dart
+++
@@ -1,6 +0,0 @@
-abstract class AuthStates{}
-
-class AuthInitial extends AuthStates{}
-class AuthLoading extends AuthStates{}
-class AuthSuccess extends AuthStates{}
-class AuthError extends AuthStates{}
